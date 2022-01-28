0e5a56863e1c3e99bb76bc091595bb0f99ffd1c6
Vaishnavi0408 <98414184+Vaishnavi0408@users.noreply.github.com>
2022-01-28 13:58:29 +0530

    Add project files.

---
+++ DemoOOPs2/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOPs2
+{
+    interface ICalculator
+    {
+        void add(int a, int b);
+        void multiply(int a, int b);
+    }
+    class CasioCalculator : ICalculator
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine("Sum="+(a+b));
+        }
+
+        public void multiply(int a, int b)
+        {
+            Console.WriteLine("Product="+a*b);
+        }
+    }
+    abstract class Teacher
+    {
+        string name;
+        string qualification;
+        int experience;
+
+        protected Teacher(string name, string qualification, int experience)
+        {
+            this.name = name;
+            this.qualification = qualification;
+            this.experience = experience;
+        }
+
+        public int Experience { get => experience; set => experience = value; }
+        public string Qualification { get => qualification; set => qualification = value; }
+        public string Name { get => name; set => name = value; }
+
+        public abstract void CalculateSalary();
+        public override string ToString()
+        {
+            return $"Name={name} qualification={qualification} experience={experience}";
+        }
+
+    }
+
+    class VisitingTeacher : Teacher
+    {
+        int hrs, rate;
+
+        public VisitingTeacher(string name, string qualification, int experience,int hrs,int rate) : base(name, qualification, experience)
+        {
+            this.hrs = hrs;
+            this.rate = rate;
+        }
+
+        public override void CalculateSalary()
+        {
+            Console.WriteLine("Salary="+hrs*rate);
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Teacher t1 = new VisitingTeacher("Deepa", "BE", 15, 50, 1000);
+            t1.CalculateSalary();
+        }
+    }
+    
+}
---
+++ DemoOOPs2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DemoOOps2
+{
+
+    class Parent
+    {
+        int a, b;
+
+        public Parent(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int B { get => b; set => b = value; }
+        public int A { get => a; set => a = value; }
+
+
+
+        public void Greet()
+        {
+            Console.WriteLine("Good Morning");
+        }
+
+        public virtual void Study()
+        {
+            Console.WriteLine("Read Book");
+        }
+
+        public override string ToString()
+        {
+            return $"a={a} b={b}";
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child(int x, int y):base(x, y)
+        {
+           // Console.WriteLine();          
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Learning online");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child c = new Child(11, 12);
+            c.Greet();
+            c.Study();
+
+            Parent p= new Parent(11,10);
+            Console.WriteLine(p);
+            p.Study();
+
+        }
+    }
+}
